d6b3b72c5bca99f7304bdc8bc91839b2bd1e0d8a
Michael House <michaelhouse@gmx.us>
2018-01-31 06:04:48 -0600

    working on osx test builds

--- configure.sh
+++ configure.sh
@@ -19,17 +19,20 @@ if [[ "$OS_NAME" == "osx" ]]; then
     pyenv install 3.5.2         || failure "couldn't install python 3.5!";
     pyenv rehash                || failure "couldn't rehash after python 3.5!";
 
-    pip install cpp-coveralls   || failure "couldn't install cpp-coveralls!";
+    pip3 install cpp-coveralls   || failure "couldn't install cpp-coveralls!";
     pyenv rehash                || failure "couldn't rehash after coveralls!";
     
 elif [[ "$OS_NAME" == "linux" ]]; then
 
     pip install --user cpp-coveralls || failure "couldn't install cpp-coveralls!";
 
-fi
-    
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-DIR=${DIR%/}/data;
+else
 
-mkdir -p ${DIR};
-export RECHAIN_HOME=${DIR};
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+    DIR=${DIR%/}/data;
+
+    mkdir -p ${DIR};
+    export RECHAIN_HOME=${DIR};
+
+fi
+ 
