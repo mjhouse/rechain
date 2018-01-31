71a0945426ac4596238c952e7a6a87b375f52fab
Michael House <michaelhouse@gmx.us>
2018-01-31 05:41:52 -0600

    working on osx test builds

--- configure.sh
+++ configure.sh
@@ -12,19 +12,19 @@ failure(){
 
 if [[ "$OS_NAME" == "osx" ]]; then 
 
-    brew install cryptopp       || failure("couldn't install cryptopp!"); 
-    rm '/usr/local/include/c++' || failure("couldn't remove symlink!");
-    brew install gcc5           || failure("couldn't install gcc5!");
-    eval "$(pyenv init -)"      || failure("couldn't init pyenv!");
-    pyenv install 3.5.2         || failure("couldn't install python 3.5!");
-    pyenv rehash                || failure("couldn't rehash after python 3.5!");
+    brew install cryptopp       || failure "couldn't install cryptopp!"; 
+    rm '/usr/local/include/c++' || failure "couldn't remove symlink!";
+    brew install gcc5           || failure "couldn't install gcc5!";
+    eval "$(pyenv init -)"      || failure "couldn't init pyenv!";
+    pyenv install 3.5.2         || failure "couldn't install python 3.5!";
+    pyenv rehash                || failure "couldn't rehash after python 3.5!";
 
-    pip install cpp-coveralls   || failure("couldn't install cpp-coveralls!");
-    pyenv rehash                || failure("couldn't rehash after coveralls!");
+    pip install cpp-coveralls   || failure "couldn't install cpp-coveralls!";
+    pyenv rehash                || failure "couldn't rehash after coveralls!";
     
 elif [[ "$OS_NAME" == "linux" ]]; then
 
-    pip install --user cpp-coveralls || failure("couldn't install cpp-coveralls!");
+    pip install --user cpp-coveralls || failure "couldn't install cpp-coveralls!";
 
 fi
     
